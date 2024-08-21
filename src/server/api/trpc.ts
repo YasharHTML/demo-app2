b1a897f9876df410533f56cbe6f45ea39cabaa5f
YASHARHTML <yashar220@hotmail.com>
2024-08-21 23:12:41 +0400

    getserversideprops

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -51,7 +51,7 @@ const createInnerTRPCContext = (opts: CreateContextOptions) => {
  *
  * @see https://trpc.io/docs/context
  */
-export const createTRPCContext = async (opts: CreateNextContextOptions) => {
+export const createTRPCContext = async (opts: Omit<CreateNextContextOptions, 'info'>) => {
   const { req, res } = opts;
 
   // Get the session from the server using the getServerSession wrapper function
